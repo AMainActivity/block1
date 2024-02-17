d1732154695999456e1dc181a3843c132d671695
Mikhail <10ama@mail.ru>
2024-02-18 00:50:58 +0500

    add buy(publication: Publication)

--- app/src/main/java/ama/test/block1/kotlin1/MainClass.kt
+++ app/src/main/java/ama/test/block1/kotlin1/MainClass.kt
@@ -1,5 +1,7 @@
 package ama.test.block1.kotlin1
 
+import org.jetbrains.annotations.NotNull
+
 class MainClass {
     /*
     * 3. Создать два объекта класса Book и один объект Magazine. Вывести в лог для каждого
@@ -19,9 +21,25 @@ class MainClass {
 
         println("сравнение по ссылке: ${book1 === book2}")
         println("сравнение по equals: ${book1 == book2}")
+
+        val bookWithNull1: Book? = null
+        val bookWithNull2: Book? = Book(price = 5.5, wordCount = 9999)
+        bookWithNull1.let { buy(bookWithNull1 as Publication) }
+        bookWithNull2.let { buy(bookWithNull2 as Publication) }
     }
 
     private fun printFormatted(classname: String, wordCount: Int, price: Double) {
         println("тип: $classname, количество строк: $wordCount, цена в евро: $price")
+    }
+
+    /*4. Создать метод buy, который в качестве параметра принимает Publication (notnull - значения)
+    * и выводит в лог “The purchase is complete. The purchase amount was [цена издания]”.
+    * Создать две переменных класса Book, в которых могут находиться null значения. Присвоить
+    * одной null, а второй любое notnull значение. Используя функцию let, попробуйте вызвать
+    * метод buy с каждой из переменных.
+    */
+    private fun buy(publication: Publication) {
+        println("The purchase is complete. The purchase amount was ${publication.price}")
+
     }
 }
