c53434f003a5b3544a503229cc223d8664753bee
Mikhail <10ama@mail.ru>
2024-03-01 21:01:14 +0500

    add activities: profile, person_data, edit;

--- app/src/main/java/ama/test/block1/ProfileInfoActivity.kt
+++ app/src/main/java/ama/test/block1/ProfileInfoActivity.kt
@@ -1,36 +1,35 @@
 package ama.test.block1
 
-import ama.test.block1.databinding.ActivityMainBinding
+import ama.test.block1.databinding.ActivityProfileInfoBinding
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.get
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class ProfileInfoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileInfoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityProfileInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-        setBottomNavigation()
         supportActionBar?.title = null
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
+        menuInflater.inflate(R.menu.info_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            android.R.id.home -> {
-                finish()
+            R.id.action_edit -> {
+                startActivity(Intent(this, ProfileEditActivity::class.java))
                 true
             }
 
@@ -38,7 +37,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setBottomNavigation() {
-        (binding.bottomNavigationView.menu.findItem(R.id.navigation_profile)).setChecked(true)
-    }
 }
